15afc746d65096df841c55b0ff942c991fa6aa50
Artem <64410376+Artiss90@users.noreply.github.com>
2021-03-11 22:37:57 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import App from './App.jsx';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
 // import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store.store}>
-      <BrowserRouter>
-        {/* <PersistGate loading={null} persistor={store.persiststore}> */}
-        <App />
-        {/* </PersistGate> */}
-      </BrowserRouter>
+      {/* <BrowserRouter> */}
+      {/* <PersistGate loading={null} persistor={store.persiststore}> */}
+      <App />
+      {/* </PersistGate> */}
+      {/* </BrowserRouter> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
